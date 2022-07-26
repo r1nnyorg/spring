71d9dd5b1a12b3db0b0a2a34453e8597b9567a41
r1nny <109706512+r1nny@users.noreply.github.com>
2022-07-25 23:46:41 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,7 +2,7 @@ public class Server
 {
     public static org.reactivestreams.Publisher<java.lang.Void> ajax(final reactor.netty.http.server.HttpServerRequest request, final reactor.netty.http.server.HttpServerResponse response)
     {
-        return response.sendString(reactor.core.publisher.Mono.just("Hello World!")));
+        return response.sendString(reactor.core.publisher.Mono.just("Hello World!"));
     }
     
     public static void main(final java.lang.String[] args)
