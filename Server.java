4933f6413dc2e01bea539218132bb9a37ab0fa78
r1nny <109706512+r1nny@users.noreply.github.com>
2022-07-25 21:18:30 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,7 +2,7 @@ public class Server
 {
     public static void main(final java.lang.String[] args)
     {
-		    final server = reactor.netty.http.server.HttpServer.create().handle((request, response) -> response.sendString(reactor.core.publisher.Mono.just("hello"))).bindNow();
+        final server = reactor.netty.http.server.HttpServer.create().handle((request, response) -> response.sendString(reactor.core.publisher.Mono.just("hello"))).bindNow();
         server.onDispose().block();
-	  }
+    }
 }
