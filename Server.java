967130f5357056e6a64fcde7a260cf0d4db69027
r1nny <109706512+r1nny@users.noreply.github.com>
2022-07-25 21:06:37 -0500

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,8 @@
+public class Server
+{
+    public static void main(final java.lang.String[] args)
+    {
+		    final server = reactor.netty.http.server.HttpServer.create().handle((request, response) -> response.sendString(reactor.core.publisher.Mono.just("hello"))).bindNow();
+        server.onDispose().block();
+	  }
+}
